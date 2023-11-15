fb253187ae51725390fb8e84c6fb875467a304d7
“efisto“ <efisto00@fesb.hr>
2023-11-15 19:15:25 +0100

    prvi zad

--- vj1.c
+++ vj1.c
@@ -1,49 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 50
-#define max_br_bod 10
+/*1. Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci.+
+Nakon toga potrebno je dinamički alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) +
+i učitati iz datoteke sve zapise.
+Na ekran ispisati ime, prezime, apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100
+*/
 
-typedef struct student{
-    char ime[MAX];
-    char prez[MAX];
-    int bodovi;
+
+
+#define VELICINAIMENA 50
+#define max_br_bodova 10
+#define ERROR 1
+
+typedef struct student {
+    char name[VELICINAIMENA];
+    char surname[VELICINAIMENA];
+    int points;
 } stud;
 
-int main()
-{
-    FILE *fp=NULL;
-    int i;
-    stud *st=NULL;
-    int br=1;
-    char c;
-    int rl=0;
-    
-    fp=fopen("dat1.txt","r");
+float relativanBrojBodova(int brojbodova) {
+    return (float)(brojbodova / max_br_bodova) * 100;
+}
 
-    if(fp == NULL){
-        printf("Ne moze se otvoriti datoteka ");
-        return 1;
+void ispisElemenata(stud* lista, int NumberOfPerson) {
+    // Ispis podataka s relativnim brojem bodova po zadanoj formuli
+    for (int i = 0; i < NumberOfPerson; i++) {
+        printf("%s %s %d, rel. broj bodova %.2f\n", lista[i].name, lista[i].surname, lista[i].points, relativanBrojBodova(lista[i].points));
+    }
+}
+
+int main() {
+    FILE* filePointer = fopen("popis.txt", "r");
+    if (filePointer == NULL) {
+        printf("Pogreška pri otvaranju datoteke.\n");
+        return ERROR;
     }
 
-    do{
-        c =fgetc(fp);
-        if( c=='\n') br++;
+    int NumberOfPerson = 0;
+    char line[200];
+    while (fgets(line, sizeof(line), filePointer) != NULL) {
+        NumberOfPerson++; // Kad dođete do kraja linije, povećate broj ljudi u listi
+    }
+    rewind(filePointer); // Vratite pokazivač datoteke na početak
 
-    }while(c != EOF);
-    
-
-    for(i=0;i<br;i++){
-        fscanf(fp,"%s %s %d",st[i].ime, st[i].prez, &st[i].bodovi );
+    stud* lista;
+    lista = (stud*)malloc(sizeof(stud) * NumberOfPerson); // Dinamički alocirana memorija za n ljudi s popisa
+    if (lista == NULL) {
+        printf("Pogreška pri alociranju memorije.\n");
+        fclose(filePointer);
+        return ERROR;
     }
 
-    for(i=0;i<br;i++){
-        rl=(st[i].bodovi*100)/max_br_bod;
-        printf("%s %s %d %d",st[i].ime, st[i].prez, st[i].bodovi ,rl );
+    // Unos podataka o osobama s liste
+    for (int i = 0; i < NumberOfPerson; i++) {
+        if (fscanf(filePointer, "%s %s %d", lista[i].name, lista[i].surname, &lista[i].points) != 3) {
+            printf("Pogreška prilikom čitanja iz datoteke.\n");
+            fclose(filePointer);
+            free(lista);
+            return ERROR;
+        }
     }
+    fclose(filePointer); // Zatvaranje datoteke, više nam nije potrebna
 
-    fclose(fp);
-    free(st);
+    // Ispis podataka s relativnim brojem bodova po zadanoj formuli
+    ispisElemenata(lista, NumberOfPerson);
+
+    // Dealociranje zauzete memorije
+    free(lista);
+
     return 0;
-
-
 }
